03da1b06e49607f81f39b69423305a177e657526
mohits-git <mohitkhatri2805@gmail.com>
2024-07-13 23:46:26 +0530

    feat(client): setup shadcn

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,12 @@
 'use client';
-import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 export default function Ping() {
+  const [connect, setConnect] = useState(false);
   useEffect(() => {
+    if (!connect) return;
     const socketConnection = io('http://localhost:8080');
     socketConnection.on('connect', () => {
       console.log('Connected to the server');
@@ -14,10 +17,15 @@ export default function Ping() {
     return () => {
       socketConnection.disconnect();
     }
-  }, []);
+  }, [connect]);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hemlo this page will connect to the websocket server at localhost:8080
+      <Button
+        onClick={() => setConnect((c) => !c)}
+      >
+        {connect ? 'Disconnect' : 'Connect'}
+      </Button>
     </main>
   );
 }
