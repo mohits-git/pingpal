187cff7eb13aaff57852195873b81a4a11cf5222
mohits-git <mohitkhatri2805@gmail.com>
2024-07-13 23:34:55 +0530

    feat(client): add socket.io-client and demo pages with quick clean up

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+
+}
+
+const Login: React.FC<Props> = () => {
+    return (
+      <div>
+        Login page
+      </div>
+    )
+}
+
+export default Login
---
+++ client/src/app/signup/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+
+}
+
+const Signup: React.FC<Props> = () => {
+    return (
+        <>
+            Signup Page
+        </>
+    )
+}
+
+export default Signup
