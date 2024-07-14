71afc133abf6529c7077a3bbafd5dbec0e3ad54b
mohits-git <mohitkhatri2805@gmail.com>
2024-07-14 12:41:05 +0530

    feat(client): add login verification on landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,12 +2,44 @@
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
+import { useRouter } from "next/navigation";
 
 export default function Ping() {
   const [connect, setConnect] = useState(false);
+  const navigate = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      navigate.push('/login');
+    }
+
+    const verifyLogin = async () => {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/verify-login`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+
+      console.log(response);
+
+      if (!response.ok) {
+        navigate.push('/login');
+      }
+    }
+
+    verifyLogin();
+  }, []);
+
   useEffect(() => {
     if (!connect) return;
-    const socketConnection = io('http://localhost:8080');
+    const socketConnection = io(process.env.NEXT_PUBLIC_BACKEND_URL as string, {
+      auth: {
+        token: localStorage.getItem('token')
+      }
+    });
     socketConnection.on('connect', () => {
       console.log('Connected to the server');
     });
@@ -18,9 +50,12 @@ export default function Ping() {
       socketConnection.disconnect();
     }
   }, [connect]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hemlo this page will connect to the websocket server at localhost:8080
+      <p>
+        Hemlo this page will connect to the websocket server at localhost:8080
+      </p>
       <Button
         onClick={() => setConnect((c) => !c)}
       >
