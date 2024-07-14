9600fdfd97de1a0f1fd5b853cb2ff029168976d4
mohits-git <mohitkhatri2805@gmail.com>
2024-07-14 18:25:27 +0530

    feat(client): add SignUp Form with implementation

---
+++ client/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,193 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { SignupSchema } from "@/lib/schemas/signup-schema";
+import { Textarea } from "@/components/ui/textarea";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+const Signup: React.FC = () => {
+  const form = useForm<z.infer<typeof SignupSchema>>({
+    resolver: zodResolver(SignupSchema),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      description: ""
+    },
+  });
+
+  const navigate = useRouter();
+
+  async function onSubmit(data: z.infer<typeof SignupSchema>) {
+    try {
+      const payload: Partial<typeof data> = {
+        ...data,
+      };
+      delete payload.confirmPassword;
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+      const responseData = await response.json();
+      if (!response.ok)
+        throw new Error(
+          responseData.email
+          || responseData.password
+          || responseData.error
+        );
+
+      const token: string = responseData.token;
+      localStorage.setItem("token", token);
+      form.reset();
+      navigate.push("/");
+    } catch (error: any) {
+      form.setError("root", {
+        message: error?.error || error?.message || "Oopsy!! :( Something went wrong ;-;"
+      });
+      toast('Oopsy!! :( Something went wrong ;-;', {
+        description: error?.error || error?.message || "Please try again later.",
+      });
+      console.log(error);
+    }
+  }
+
+  return (
+    <Card className="w-full max-w-md">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <CardHeader>
+            <CardTitle className="text-xl">Signup</CardTitle>
+            <CardDescription>Signup to Ping your Pals.</CardDescription>
+            {form.formState.errors.root && (
+              <div className="text-red-500 text-sm">{form.formState.errors.root.message}</div>
+            )}
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your name..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter a unique username..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Tell others about yourself in short..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your email..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Create a new password..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Confirm Password..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <Button className="w-full" type="submit">Signup</Button>
+          </CardFooter>
+        </form>
+      </Form>
+    </Card>
+  )
+}
+
+export default Signup;
